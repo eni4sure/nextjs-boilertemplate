8adbcf0f6274d8e3eb73c146d050a18b53098604
Eniola Osabiya <eosabiya@gmail.com>
2022-11-01 09:03:52 +0100

    refactor/update: useUser Hook update

--- utils/use-user.ts
+++ utils/use-user.ts
@@ -1,20 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+import { getCookie, hasCookie } from "cookies-next";
 
 import { userGetMe } from "../http";
 
 const useUser = () => {
     const {
+        isError,
         isLoading,
         isRefetching,
-        data: User
+        data: User,
+        refetch
     } = useQuery(["auth-user"], userGetMe, {
         cacheTime: Infinity,
         staleTime: 60000 * 10 /* 10 mins */
     });
 
+    if (isError && hasCookie("auth-token")) {
+        const decodeJwt: JwtPayload = jwtDecode((getCookie("auth-token") as string) || "");
+        delete decodeJwt.iat;
+        delete decodeJwt.exp;
+
+        return {
+            isLoadingUser: isLoading || isRefetching,
+            user: decodeJwt || null
+        };
+    }
+
     return {
         isLoadingUser: isLoading || isRefetching,
-        user: User?.data.data || null
+        user: User?.data.data || null,
+        refreshAuthState: refetch
     };
 };
 
