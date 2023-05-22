7f1d561ebb0391949f91f4cdd493451918bca6a0
Eniola Osabiya <eosabiya@gmail.com>
2023-05-21 23:41:22 -0500

    chore: update http api to match backend

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,7 @@ import $http from "./xhr";
 
 export const authLogin = async (data: any) => $http.post("/auth/login", data).then((res) => res.data);
 export const authRegister = async (data: any) => $http.post("/auth/register", data).then((res) => res.data);
-export const authVerifyEmail = async (data: any) => await $http.post("/auth/verify-email", { userId: data.userId, verificationToken: data.verificationToken, verificationCode: data.verificationCode }).then((res) => res.data);
+export const authVerifyEmail = async (data: any) => await $http.post("/auth/verify-email", { userId: data.userId, verificationToken: data.verificationToken }).then((res) => res.data);
 export const authRequestEmailVerification = async (data: any) => await $http.post("/auth/request-email-verification", { userId: data.userId }).then((res) => res.data);
 export const authRequestPasswordReset = async (data: any) => await $http.post("/auth/request-password-reset", { email: data.email }).then((res) => res.data);
 export const authResetPassword = async (data: any) => await $http.patch("/auth/reset-password", { userId: data.userId, resetToken: data.resetToken, newPassword: data.newPassword }).then((res) => res.data);
