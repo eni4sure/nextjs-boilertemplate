5e87958c183e54dd7b4c4e5ccb0681279eb3ec02
Eniola Osabiya <eosabiya@gmail.com>
2023-06-23 23:07:21 -0500

    chore: update auth guards to functional components

--- src/utils/auth-guard.tsx
+++ src/utils/auth-guard.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Router from "next/router";
 import { hasCookie } from "cookies-next";
-import type { NextPage, NextPageContext } from "next";
+import { NextPage, NextPageContext } from "next";
 
 // Custom function to handle Redirect during getInitialProps
 export const Redirect = (context: NextPageContext, target: string) => {
     if (context.res) {
         // If this is server, return a temp redirect code with new location
-        context.res.writeHead(302, { Location: target });
+        context.res.writeHead(307, { Location: target });
         context.res.end();
     } else {
         // In the browser, we just replace the route
@@ -17,42 +17,54 @@ export const Redirect = (context: NextPageContext, target: string) => {
     return {};
 };
 
-export const withAuth = <T extends object>(WrappedComponent: NextPage<T>) => {
-    return class AuthenticatedPage extends React.Component<T> {
-        static async getInitialProps(context: NextPageContext) {
-            // If the auth-token doesn't exist, redirect to /login
-            if (!hasCookie("auth-token", context)) {
-                Redirect(context, `/auth/login?next=${context.asPath}`);
-            }
+export const withAuth = <P extends object>(WrappedComponent: React.ComponentType<P>) => {
+    const WithAuthLogic: NextPage<P, {}> = (props) => {
+        // Add additional props or modify existing props
+        const modifiedProps = {
+            ...props,
+        };
 
-            return { query: context.query };
-        }
-
-        render() {
-            return <WrappedComponent {...this.props} />;
-        }
+        // Return the wrapped component with modified props
+        return <WrappedComponent {...modifiedProps} />;
     };
+
+    WithAuthLogic.getInitialProps = async (context: NextPageContext) => {
+        // If the auth-token doesn't exist, redirect to /login
+        if (!hasCookie("auth-token", context)) {
+            return Redirect(context, `/auth/login?next=${context.asPath}`);
+        }
+
+        return { query: context.query };
+    };
+
+    return WithAuthLogic;
 };
 
-export const withoutAuth = <T extends object>(WrappedComponent: NextPage<T>) => {
-    return class UnAuthenticatedPage extends React.Component<T> {
-        static async getInitialProps(context: NextPageContext) {
-            // If the auth-token exist
-            if (hasCookie("auth-token", context)) {
-                // If the there's a pending next route, go there.
-                if (context.query && context.query.next) {
-                    Redirect(context, `${context.query.next}`);
-                }
+export const withoutAuth = <P extends object>(WrappedComponent: React.ComponentType<P>) => {
+    const WithoutAuthLogic: NextPage<P, {}> = (props) => {
+        // Add additional props or modify existing props
+        const modifiedProps = {
+            ...props,
+        };
 
-                // Go to dashboard
-                Redirect(context, "/dashboard");
+        // Return the wrapped component with modified props
+        return <WrappedComponent {...modifiedProps} />;
+    };
+
+    WithoutAuthLogic.getInitialProps = async (context: NextPageContext) => {
+        // If the auth-token exist
+        if (hasCookie("auth-token", context)) {
+            // If the there's a pending next route, go there.
+            if (context.query && context.query.next) {
+                return Redirect(context, `${context.query.next}`);
             }
 
-            return { query: context.query };
+            // Go to dashboard
+            return Redirect(context, "/dashboard");
         }
 
-        render() {
-            return <WrappedComponent {...this.props} />;
-        }
+        return { query: context.query };
     };
+
+    return WithoutAuthLogic;
 };
