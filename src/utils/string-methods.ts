d28fd525f7ba147ad09ad75d65f3c93b67c478f1
Eniola Osabiya <eosabiya@gmail.com>
2023-06-28 15:23:47 -0500

    chore: type typo update

--- src/utils/string-methods.ts
+++ src/utils/string-methods.ts
@@ -1,3 +1,3 @@
-export const formatFirstLetterToUppercase = (string: String) => {
+export const formatFirstLetterToUppercase = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
